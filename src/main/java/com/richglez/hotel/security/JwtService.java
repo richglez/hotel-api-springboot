2d0532774f538160e0f2b8059f26e62189bf0d31
rich <rickygb082002@gmail.com>
2026-05-14 23:22:40 -0600

    feat: JwtService security

--- src/main/java/com/richglez/hotel/security/JwtService.java
+++ src/main/java/com/richglez/hotel/security/JwtService.java
@@ -38,7 +38,30 @@ public class JwtService {
                 .signWith(getSigninKey(), SignatureAlgorithm.HS256) // 5. FIRMAMOS con nuestra Key
                 .compact(); // 6. "Empaquetamos" todo en un String
     }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigninKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername());
+    }
 
+    private boolean isTokenExpired(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigninKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
 }
+
+
