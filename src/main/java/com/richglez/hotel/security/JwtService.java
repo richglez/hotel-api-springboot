26d085179204f84183eb4e68c0acf73153add107
rich <rickygb082002@gmail.com>
2026-05-14 22:31:36 -0600

    feat: JWT secret format cripto key signin, generate token

--- src/main/java/com/richglez/hotel/security/JwtService.java
+++ src/main/java/com/richglez/hotel/security/JwtService.java
@@ -1,7 +1,17 @@
 package com.richglez.hotel.security;
 
-import lombok.Value;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Service
 public class JwtService {
@@ -11,4 +21,24 @@ public class JwtService {
 
     @Value("${jwt.expiration}")
     private long expiration; // en milisegundos
+
+    // transformar el secreto en un formato criptográfico (Key) para firmar
+    private Key getSigninKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes()); // (Hashed Message Authentication Code).
+    } // HMAC SHA
+
+    // El metodo para generar una carta con un destinatario, informacion y sello/firma
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername()) // 2. Agregamos datos (Payload)
+                .setIssuedAt(new Date()) // 3. Fecha de inicio
+                .setExpiration(new Date(System.currentTimeMillis() + expiration)) // 4. Expira en ...
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256) // 5. FIRMAMOS con nuestra Key
+                .compact(); // 6. "Empaquetamos" todo en un String
+    }
+    }
+
+
 }
