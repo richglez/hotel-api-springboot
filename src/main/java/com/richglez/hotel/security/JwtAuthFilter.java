096b2a54f2e2bce3c32a387a668d0ea5db3f3230
rich <rickygb082002@gmail.com>
2026-05-17 16:06:25 -0600

    feat: jwtAuthFilter implementation

--- src/main/java/com/richglez/hotel/security/JwtAuthFilter.java
+++ src/main/java/com/richglez/hotel/security/JwtAuthFilter.java
@@ -1,8 +1,18 @@
 package com.richglez.hotel.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,4 +20,36 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response); // pasa estos atributos al siguiente filtro
+            return; // se acaba el filtro aqui
+        }
+
+        final String token = authHeader.substring(7);
+        final String email = jwtService.extractUsername(token);
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+            if (jwtService.isTokenValid(token, userDetails)) {
+                var authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
+
+
+// Seguridad
--- src/main/java/com/richglez/hotel/security/JwtService.java
+++ src/main/java/com/richglez/hotel/security/JwtService.java
@@ -64,4 +64,4 @@ public class JwtService {
     }
 }
 
-
+// Token logistic
