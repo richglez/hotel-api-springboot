fe47e9fab4857052c06d5d8ec03d1df570833b27
rich <rickygb082002@gmail.com>
2026-05-19 15:53:54 -0600

    frontend structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import ClientList from "./components/ClientList"
+import Navbar from "./components/common/Navbar/Navbar.tsx";
 
 function App() {
     return (
         <div>
+            <Navbar></Navbar>
             <ClientList></ClientList>
-        </div>)
+        </div>
+    )
 }
 
 export default App;
---
+++ frontend/src/components/common/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className={}>
+            <div className="logo">Hotel-Reservations</div>
+            <ul>
+                <li><a href="/frontend/public">Home</a></li>
+                <li><a href="/rooms">Rooms</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
