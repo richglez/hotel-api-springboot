c4eaaa7c484d651c3d10aed23deb262b78b05ede
rich <rickygb082002@gmail.com>
2026-05-23 15:02:53 -0600

    feat: login, register dto fix notnull to notblank to string fields

--- src/main/java/com/richglez/hotel/auth/LoginRequest.java
+++ src/main/java/com/richglez/hotel/auth/LoginRequest.java
@@ -1,13 +1,13 @@
 package com.richglez.hotel.auth;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
-    @NotNull(message = "Email is required")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
     private String password;
 }
--- src/main/java/com/richglez/hotel/auth/RegisterRequest.java
+++ src/main/java/com/richglez/hotel/auth/RegisterRequest.java
@@ -1,20 +1,22 @@
 package com.richglez.hotel.auth;
 
-import com.richglez.hotel.enums.Roles;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
-    @NotNull(message = "Name is required")
+    @NotBlank(message = "Name is required")
     private String name;
-    @NotNull(message = "Last name is required")
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @NotNull(message = "Email is required")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must have at least 8 characters")
     private String password;
 
     private String phone;
