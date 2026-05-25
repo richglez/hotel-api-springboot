cc9871b25e9d63b4933019eba6b22f97ae9cc19d
rich <rickygb082002@gmail.com>
2026-05-25 00:40:28 -0600

    feat: AuthContextProvider

--- frontend/src/features/auth/context/AuthContext.tsx
+++ frontend/src/features/auth/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import  { createContext, useContext, useState, type ReactNode } from "react";
+import {createContext, useContext, useState, type ReactNode} from "react";
 
 type AuthContextType = {
     token: string | null;
@@ -6,3 +6,33 @@ type AuthContextType = {
     login: (token: string) => void;
     logout: () => void;
 };
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+
+
+// El objeto debe tener una propiedad children que sea renderizable por React
+// “del objeto recibido, saca la propiedad children
+export const AuthProvider = ({children}: { children: ReactNode }) => {
+    const [token, setToken] = useState<string | null>(() => {
+        return localStorage.getItem("token")
+    });
+
+    const login = (newToken: string) => {
+        localStorage.setItem("token", newToken);
+        setToken(newToken)
+    }
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setToken(null);
+    }
+
+
+
+    return (
+        <AuthContext.Provider></AuthContext.Provider>
+    )
+
+
+}
