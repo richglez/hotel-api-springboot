3e78b6b07bcfa463902a3720709f13dc724fae57
rich <rickygb082002@gmail.com>
2026-06-04 23:35:50 -0600

    feat: ReservationControllerIntegrationTest.java implementation

--- src/test/java/com/richglez/hotel/reservations/controller/ReservationControllerIntegrationTest.java
+++ src/test/java/com/richglez/hotel/reservations/controller/ReservationControllerIntegrationTest.java
@@ -1,28 +1,38 @@
 package com.richglez.hotel.reservations.controller;
 
+import lombok.With;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+
+@SpringBootTest // load beans, services, repo, security, config
+@AutoConfigureMockMvc // Spring prepara el simulador HTTP.
+@ActiveProfiles("test") // usa application-test.properties
 public class ReservationControllerIntegrationTest {
 
+    // Inject dependencies contructor
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc; // Simulates real HTTP without open a real server
 
     @Test
+    @WithMockUser(roles = "ADMIN")
     void shouldReturnReservations() throws Exception {
 
-        mockMvc.perform(get("/api/reservations"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/reservations")) // fake request
+                .andExpect(status().isOk()); // valida el status espero HTTP 200
     }
 }
+
+//Cuando hago GET /api/reservations
+//la API debe responder 200 OK
